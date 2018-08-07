60462e15be78a485262ef9166bd7a0033d10ddd7
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-08-06 18:10:04 -0700

    Energy loss pdf

--- src/KryptonPdf/EnergyLossPdf.cxx
+++ src/KryptonPdf/EnergyLossPdf.cxx
@@ -14,25 +14,43 @@ ClassImp(EnergyLossPdf);
 // {
 
 EnergyLossPdf::EnergyLossPdf(const char *name, const char *title,
-                             RooAbsReal &_KE, RooAbsReal &_mean, RooAbsReal &_HWHM, RooAbsReal &_alpha) : RooAbsPdf(name, title),
-                                                                                                          KE("KE", "KE", this, _KE),
-                                                                                                          mean("mean", "Mean", this, _mean),
-                                                                                                          HWHM("HWHM", "HWHM", this, _HWHM),
-                                                                                                          alpha("alpha", "Alpha", this, _alpha)
+                             RooAbsReal &_x,
+                             RooAbsReal &_mean1, RooAbsReal &_mean2,
+                             RooAbsReal &_width1, RooAbsReal &_width2,
+                             RooAbsReal &_amplitude1, RooAbsReal &_amplitude2) : RooAbsPdf(name, title),
+                                                                                 x("x", "x", this, _x),
+                                                                                 mean1("mean1", "Mean1", this, _mean1),
+                                                                                 mean2("mean2", "Mean2", this, _mean2),
+                                                                                 width1("width1", "Width1", this, _width1),
+                                                                                 width2("width2", "Width2", this, _width2),
+                                                                                 amplitude1("amplitude1", "Amplitude1", this, _amplitude1),
+                                                                                 amplitude2("amplitude2", "Amplitude2", this, _amplitude2),
+                                                                                 epsilonc("epsilonc", "Epsilon cut", this, _epsilonc)
 {
 }
 
 EnergyLossPdf::EnergyLossPdf(const EnergyLossPdf &other, const char *name) : RooAbsPdf(other, name),
                                                                              KE("KE", this, other.KE),
-                                                                             mean("mean", this, other.mean),
-                                                                             HWHM("HWHM", this, other.HWHM),
-                                                                             alpha("alpha", this, other.alpha)
+                                                                             mean1("mean1", this, other.mean1),
+                                                                             mean2("mean2", this, other.mean2),
+                                                                             width1("width1", this, other.width1),
+                                                                             width2("width2", this, other.width2),
+                                                                             amplitude1("amplitude1", this, other.amplitude1),
+                                                                             amplitude2("amplitude2", this, other.amplitude2),
+                                                                             epsilonc("epsilonc", this, other.epsilonc)
 {
 }
 
 Double_t EnergyLossPdf::evaluate() const
 {
-    Double_t arg = KE - mean;
-    return 1. / (TMath::Pi() * HWHM) / TMath::Power(1. + TMath::Power(arg / HWHM, 2), (1. - alpha) / 2.) * (TMath::Cos(TMath::Pi() * alpha / 2. + (1. - alpha) * TMath::ATan(arg / HWHM)));
+    if (x < epsilonc)
+    {
+        return amplitude1 / TMath::Gaus(x, mean1, sigma1);
+    }
+    else
+    {
+        // return amplitude2 * TMath::CauchyDist;
+        return 0;
+    }
 }
 // }
--- src/KryptonPdf/EnergyLossPdf.hh
+++ src/KryptonPdf/EnergyLossPdf.hh
@@ -13,7 +13,7 @@ class EnergyLossPdf : public RooAbsPdf
 public:
   EnergyLossPdf(){};
   EnergyLossPdf(const char *name, const char *title,
-                        RooAbsReal &_KE, RooAbsReal &_mean, RooAbsReal &_HWHM, RooAbsReal &_alpha);
+                RooAbsReal &_x, RooAbsReal &_mean1, RooAbsReal &_mean2, RooAbsReal &_width1, RooAbsReal &_width2, RooAbsReal &_amplitude1, RooAbsReal &_amplitude2, RooAbsReal &_epsilonc);
   EnergyLossPdf(const EnergyLossPdf &other, const char *name = 0);
   virtual TObject *clone(const char *newname) const { return new EnergyLossPdf(*this, newname); }
   inline virtual ~EnergyLossPdf(){};
@@ -21,10 +21,14 @@ public:
   Double_t evaluate() const;
 
 protected:
-  RooRealProxy KE;
-  RooRealProxy mean;
-  RooRealProxy HWHM;
-  RooRealProxy alpha; //Asymmetry coefficient
+  RooRealProxy x;
+  RooRealProxy mean1;
+  RooRealProxy mean2;
+  RooRealProxy width1;
+  RooRealProxy width2;
+  RooRealProxy amplitude1;
+  RooRealProxy amplitude2;
+  RooRealProxy epsilonc;
 
 public:
   ClassDef(EnergyLossPdf, 1)
